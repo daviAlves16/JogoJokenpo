fa65c9027d48ab15752add3f9a80dc28f43906d6
Davi Alves <daviice2011@hotmail.com>
2022-08-20 16:01:12 -0300

    segundo Commit

---
+++ jokenpo/src/pages/App/App.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export function App() {
+  return (
+    <div>
+      <h1>Aquiiiii</h1>
+
+    </div>
+  )
+}
+
---
+++ jokenpo/src/pages/tela1/Tela1.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export function Tela1() {
+  return (
+    <div className='base'>
+        <h1>Tela 1</h1>
+    </div>
+  )
+}
